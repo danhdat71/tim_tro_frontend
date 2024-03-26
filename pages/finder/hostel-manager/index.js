90fade984ac2ac8fc0f58330f6aba8729cbb5dcb
Bap-datd <datd@bap.jp>
2024-03-26 15:07:09 +0700

    Product manager

--- pages/finder/hostel-manager/index.js
+++ pages/finder/hostel-manager/index.js
@@ -1,9 +1,42 @@
+import Breadcrumb from '@/components/breadcrumb/breadcrumb';
+import TitleLeftBig from '@/components/titles/title-left-big/title-left-big';
 import React from 'react';
+import cl from './hostel-manager.module.css';
+import ProductProcessing from '@/components/product-processing/product-processing';
+
+const breadcrumbs = [
+    {
+        label: 'Trang chủ',
+        href: '/'
+    },
+    {
+        label: 'Quản lý tìm trọ',
+        href:'/finder/hostel-manager'
+    }
+];
 
 const Index = () => {
     return (
-        <div>
-            123
+        <div className={cl.manager}>
+            <Breadcrumb items={breadcrumbs}></Breadcrumb>
+            <TitleLeftBig title="Quản lý tìm trọ"></TitleLeftBig>
+            <div className={cl.button_bar}>
+                <button className={`${cl.button} ${cl.active}`}>
+                    <span>Đang yêu cầu</span>
+                    <span>(0)</span>
+                </button>
+                <button className={`${cl.button}`}>
+                    <span>Đang chờ xem</span>
+                    <span>(0)</span>
+                </button>
+                <button className={`${cl.button}`}>
+                    <span>Đã xem</span>
+                    <span>(0)</span>
+                </button>
+            </div>
+            <div>
+                <ProductProcessing></ProductProcessing>
+            </div>
         </div>
     );
 }
