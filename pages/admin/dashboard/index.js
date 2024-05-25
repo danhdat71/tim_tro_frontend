6fb922cd7c056861aa98dac6fd2be5b772a9dc71
Bap-datd <datd@bap.jp>
2024-05-25 21:26:59 +0700

    Admin chart

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -24,7 +24,20 @@ ChartJS.register(
     Legend,
 );
 
-const labels = ['Tháng 1', 'Tháng 2'];
+const labels = [
+    'Tháng 1',
+    'Tháng 2',
+    'Tháng 3',
+    'Tháng 4',
+    'Tháng 5',
+    'Tháng 6',
+    'Tháng 7',
+    'Tháng 8',
+    'Tháng 9',
+    'Tháng 10',
+    'Tháng 11',
+    'Tháng 12',
+];
 
 export const data = {
     labels,
