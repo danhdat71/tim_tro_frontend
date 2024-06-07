0b429ea68782b0d3c9b37a068c48bf8095aea629
Bap-datd <datd@bap.jp>
2024-06-07 23:12:41 +0700

    Fix bug

--- components/header/header-notifications/header-notifications.js
+++ components/header/header-notifications/header-notifications.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import cl from './header-notifications.module.css';
 import Link from 'next/link';
 import { useAppSelector } from '@/redux/store';
+import { useOutsideAlerter } from '@/hooks/useOutsideClick';
+import { useDispatch } from 'react-redux';
+import { toggleNotificationBox } from '@/redux/features/header';
 
 const HeaderNotifications = (props) => {
-
+    const dispatch = useDispatch();
     const headerState = useAppSelector(function(state){
         return state.headerReducer.header;
     });
 
+    function handleSetEnableHeaderNotificationsBox(status) {
+        dispatch(toggleNotificationBox(status));
+    }
+
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef, function(){
+        handleSetEnableHeaderNotificationsBox(!headerState.is_enable_notification_box);
+    });
+
     return (
         <div
+            ref={wrapperRef}
             className={`${cl.wrap_notifications} ${headerState.is_enable_notification_box ? cl.show : false}`}
         >
             <div className={`scrollbar ${cl.list}`}>
