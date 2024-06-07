4f0e2c62f3b6762da28fd114ca60cf9e9b9ce6a7
Bap-datd <datd@bap.jp>
2024-06-07 09:54:44 +0700

    Price

--- pages/price-locale/index.js
+++ pages/price-locale/index.js
@@ -2,6 +2,7 @@ import TitleLeftBig from '@/components/titles/title-left-big/title-left-big';
 import React from 'react';
 import Select from 'react-select';
 import cl from './index.module.css';
+import headerBackground from '@/assets/imgs/background_price_table.png';
 
 const provinces =[
     {'label':'Tỉnh/thành', 'value': 1}
@@ -12,37 +13,100 @@ const district =[
 
 const Index = () => {
     return (
-        <div>
-            <div className={cl.title_big}>Thống kê giá nhà đất theo khu vực - Tháng 5/2024</div>
-            <div className={cl.title_sub}>Bảng giá trung bình theo tỉnh thành, quận huyện. Được tổng hợp từ dữ liệu các nguồn tin đăng trong 3 tháng đến nay. Đơn vị tính giá/m<sup>2</sup> so với cùng kì tháng trước.</div>
-            <div className={cl.group_search}>
-                <Select
-                    onChange={(selectedOption)=>{
-                    }}
-                    options={provinces}
-                    maxMenuHeight={190}
-                    value={provinces[0]}
-                ></Select>
+        <div className={cl.wrap_main}>
+            <div className={cl.header_bg}>
+                <img src={headerBackground.src}></img>
             </div>
-            <div className={cl.wrap_list}>
-                <div className={cl.item}>
-                    <div>Hà Nội</div>
-                    <div>Giá: 2.5tr/m<sup>2</sup></div>
-                    <div>
-                        <span className={`${cl.grow_icon} ${cl.grow_icon_down}`}>
-                            <i class="fas fa-sort-down"></i>
-                        </span>
-                        <span>Giảm 5%</span>
-                    </div>
+            <div className={cl.main}>
+                <div className={cl.title_big}>Thống kê giá nhà đất theo khu vực - Tháng 5/2024</div>
+                <div className={cl.title_sub}>Bảng giá trung bình theo tỉnh thành, quận huyện. Được tổng hợp từ dữ liệu các nguồn tin đăng trong 3 tháng đến nay. Đơn vị tính giá/m<sup>2</sup> so với cùng kì tháng trước.</div>
+                <div className={cl.group_search}>
+                    <Select
+                        onChange={(selectedOption)=>{
+                        }}
+                        options={provinces}
+                        maxMenuHeight={190}
+                        value={provinces[0]}
+                    ></Select>
                 </div>
-                <div className={cl.item}>
-                    <div>TP.HCM</div>
-                    <div>Giá: 2.5tr/m<sup>2</sup></div>
-                    <div>
-                        <span className={`${cl.grow_icon} ${cl.grow_icon_up}`}>
-                            <i class="fas fa-sort-up"></i>
-                        </span>
-                        <span>Tăng 5%</span>
+                <div className={cl.wrap_list}>
+                    <div className={cl.item}>
+                        <div>Quận 1</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_down}`}>
+                                <i class="fas fa-sort-down"></i>
+                            </span>
+                            <span>Giảm 5%</span>
+                        </div>
+                    </div>
+                    <div className={cl.item}>
+                        <div>Quận Bình Tân</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_up}`}>
+                                <i class="fas fa-sort-up"></i>
+                            </span>
+                            <span>Tăng 5%</span>
+                        </div>
+                    </div>
+                    <div className={cl.item}>
+                        <div>Quận Bình Thạnh</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_up}`}>
+                                <i class="fas fa-sort-up"></i>
+                            </span>
+                            <span>Tăng 5%</span>
+                        </div>
+                    </div>
+                    <div className={cl.item}>
+                        <div>Quận Thủ Đức</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_up}`}>
+                                <i class="fas fa-sort-up"></i>
+                            </span>
+                            <span>Tăng 5%</span>
+                        </div>
+                    </div>
+                    <div className={cl.item}>
+                        <div>Quận 4</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_up}`}>
+                                <i class="fas fa-sort-up"></i>
+                            </span>
+                            <span>Tăng 5%</span>
+                        </div>
+                    </div>
+                    <div className={cl.item}>
+                        <div>Quận 5</div>
+                        <div>
+                            <span>Giá: </span>
+                            <span>2.5tr/m<sup>2</sup></span>
+                        </div>
+                        <div>
+                            <span className={`${cl.grow_icon} ${cl.grow_icon_down}`}>
+                                <i class="fas fa-sort-down"></i>
+                            </span>
+                            <span>Giảm 5%</span>
+                        </div>
                     </div>
                 </div>
             </div>
