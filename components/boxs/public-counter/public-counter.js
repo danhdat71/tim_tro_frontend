7729d23e629e9f4223462a457ebb3df0944a7240
Bap-datd <datd@bap.jp>
2024-06-09 15:37:03 +0700

    Counter

--- components/boxs/public-counter/public-counter.js
+++ components/boxs/public-counter/public-counter.js
@@ -1,24 +1,54 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import cl from './public-counter.module.css';
+import CountUp from 'react-countup';
+import axios from '@/helpers/http-requests/axios';
 
 const PublicCounter = () => {
+
+    let [counterData, setCounterData] = useState({});
+
+    useEffect(function(){
+        axios.get('/system-counter', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        })
+        .then(function(res){
+            if (res.status == 200) {
+                setCounterData(res.data);
+            }
+        });
+    }, []);
+
     return (
         <div className={cl.public_counter}>
             <div className={cl.item}>
                 <span><i className="fas fa-circle"></i> Phòng trọ:</span>
-                <span>10.000</span>
+                <CountUp
+                    end={counterData?.hostel_count || 0}
+                    duration={5}
+                />
             </div>
             <div className={cl.item}>
                 <span><i className="fas fa-circle"></i> Nhà nguyên căn:</span>
-                <span>10.000</span>
+                <CountUp
+                    end={counterData?.full_house_count || 0}
+                    duration={5}
+                />
             </div>
             <div className={cl.item}>
                 <span><i className="fas fa-circle"></i> Chung cư:</span>
-                <span>10.000</span>
+                <CountUp
+                    end={counterData?.apartment_count || 0}
+                    duration={5}
+                />
             </div>
             <div className={cl.item}>
                 <span><i className="fas fa-circle"></i> Ở ghép:</span>
-                <span>10.000</span>
+                <CountUp
+                    end={counterData?.together_count || 0}
+                    duration={5}
+                />
             </div>
         </div>
     );
