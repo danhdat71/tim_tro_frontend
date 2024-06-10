8ab22a1f51f3bc7a7a0c76e9bd6168826f5d0f73
Bap-datd <datd@bap.jp>
2024-06-10 22:14:07 +0700

    Fix bugs ui

--- layouts/layout.js
+++ layouts/layout.js
@@ -9,6 +9,7 @@ const Layout = (props) => {
     {
         return (
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
                 {/* Google font */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
